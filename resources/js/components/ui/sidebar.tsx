2652ccd39f207e6787e35390d4e5051d9ea6ff50
Miftahul Huda <miftahul21si@mahasiswa.pcr.ac.id>
2025-08-13 19:06:03 +0700

    Update sidebar button styles for improved accessibility and visual consistency

--- resources/js/components/ui/sidebar.tsx
+++ resources/js/components/ui/sidebar.tsx
@@ -473,7 +473,7 @@ const sidebarMenuButtonVariants = cva(
   {
     variants: {
       variant: {
-        default: "hover:bg-[#E0E0E0] hover:text-black",
+        default: "hover:bg-[#e3ecf6] hover:text-primary dark:hover:bg-muted dark:hover:text-white",
         outline:
           "bg-background shadow-[0_0_0_1px_hsl(var(--sidebar-border))] hover:bg-sidebar-accent hover:text-sidebar-accent-foreground hover:shadow-[0_0_0_1px_hsl(var(--sidebar-accent))]",
       },
